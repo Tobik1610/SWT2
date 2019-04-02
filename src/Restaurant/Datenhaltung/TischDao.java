d52a93636daac78b97c47c2be4befcf8fc65a24c
Tobias Krämer <tobias.kraemer@trilux.com>
2019-04-02 13:54:12 +0200

    TischDao initialisieren der Daten

--- src/Restaurant/Datenhaltung/TischDao.java
+++ src/Restaurant/Datenhaltung/TischDao.java
@@ -1,20 +1,37 @@
 package Restaurant.Datenhaltung;
 
+import java.util.ArrayList;
 import java.util.List;
 import Restaurant.Fachlogik.Tischverwaltung.Tisch;
 
 public class TischDao implements ITischDao{
+	
+	private List<Tisch> tische;
+	
+	public TischDao() {
+		initDaten();
+	}
 
 	@Override
 	public void speichern(List<Tisch> tische) {
-		// TODO Auto-generated method stub
+		this.tische = tische;
 		
 	}
 
 	@Override
 	public List<Tisch> laden() {
-		// TODO Auto-generated method stub
-		return null;
+		return tische;
+	}
+	
+	public void initDaten() {
+		tische = new ArrayList<>();
+		
+		tische.add(new Tisch(1,6));
+		tische.add(new Tisch(2,2));
+		tische.add(new Tisch(3,6));
+		tische.add(new Tisch(4,6));
+		tische.add(new Tisch(5,4));
+		tische.add(new Tisch(6,10));
 	}
 
 }
